b854864000a3ae366b8ea17f7a8eaa6583c7cc76
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-03 16:49:14 -0500

    Create farmers.php

---
+++ farmers.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-farmers.php");
+
+$pageTitle="Farmers";
+include "view-header.php";
+$farmers= selectFarmers();
+include "view-farmers.php";
+include "view-footer.php";
+?>
