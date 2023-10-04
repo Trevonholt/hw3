9753f5d39735f57571f591348adcc3548994d20b
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-03 22:26:02 -0500

    Create view-farmers.php

---
+++ view-farmers.php
@@ -0,0 +1,25 @@
+<h1>Farmers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Phone Number</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($farmer=$farmers->fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $farmer['farmer_id']; ?></td>
+    <td><?php echo $farmer['farmer_name']; ?></td>
+    <td><?php echo $farmer['phone_number']; ?></td>
+  </tr>
+<php
+}
+?>
+    </tbody>
+  </table>
+</div>
