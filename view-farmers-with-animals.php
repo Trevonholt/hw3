58e46484968bd73983ffe88e6d41efd42e8dae71
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-04 00:43:01 -0500

    Update view-farmers-with-animals.php

--- view-farmers-with-animals.php
+++ view-farmers-with-animals.php
@@ -7,13 +7,16 @@ while ($farmer=$farmers->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $farmer['farmer_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
   $animals = selectAnimalsByFarmer($farmer['farmer_id']);
   while ($animal=$animals->fetch_assoc()){
 ?>
+    <li class="list-group-item"><?php echo $animal['animal_id']; ?> - <?php echo $animal['type']; ?> - <?php echo $animal['breed']; ?> - <?php echo $animal['animal_count']; ?></li>
 <?php
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Phone Number: <?php echo $farmer['phone_number']; ?></small></p>
     </div>
