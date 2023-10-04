18e9721edfa0c131ab9ddacf3e251ec7a5348b7c
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-03 23:26:37 -0500

    Update model-animals-for-farmers.php

--- model-animals-for-farmers.php
+++ model-animals-for-farmers.php
@@ -2,7 +2,7 @@
 function selectAnimalsForFarmers($fid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT a.animal_id, type, breed, animal_count, description, name as "Manager_Name", age FROM `Animals` a JOIN Manager m on a.animal_id=m.manager_id where m.animal_id=?");
+        $stmt = $conn->prepare("SELECT a.animal_id, type, breed, animal_count, description, name, age FROM `Animals` a JOIN Manager m on a.animal_id=m.manager_id where m.animal_id=?");
         $stmt->bind_param("i", $fid);
         $stmt->execute();
         $result = $stmt->get_result();
