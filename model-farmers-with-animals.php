90898e35924b7f2e45e845120210e9ea4a5039ef
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-04 00:28:34 -0500

    Update model-farmers-with-animals.php

--- model-farmers-with-animals.php
+++ model-farmers-with-animals.php
@@ -12,4 +12,19 @@ function selectFarmers() {
         throw $e;
     }
 }
+
+function selectLandWithAnimals($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT a.animal_id, type, breed, animal_count, description, name, age FROM `Animals` a JOIN Manager m on a.animal_id=m.manager_id where m.land_id=?");
+        $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
