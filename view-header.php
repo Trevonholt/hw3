72894c37b4e24af55a3f4453fadf5e581295dc6b
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-03 22:12:21 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="farmer.php">Farmers</a>
+          <a class="nav-link" href="farmers.php">Farmers</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="animal.php">Animals</a>
