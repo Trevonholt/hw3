367cc3e6f0891b7a00a27c619835aba5dc9001d7
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-03 23:56:09 -0500

    Update view-land.php

--- view-land.php
+++ view-land.php
@@ -16,10 +16,12 @@ while ($land=$lands->fetch_assoc()) {
     <td><?php echo $land['land_id']; ?></td>
     <td><?php echo $land['city']; ?></td>
     <td><?php echo $land['state']; ?></td>
+    <td>
      <form method="post" action="land-with-animals.php>
         <input type="hidden" name="aid" value="<?php echo $land['land_id']; ?>">
         <button type="submit" class="btn btn-primary">Lands</button>
       </form>
+    </td>
   </tr>
 <?php
 }
