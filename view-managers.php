cb501baebd0fa2bf9dd7d4776a26eb4e3587cd91
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 10:35:44 -0500

    Update view-managers.php

--- view-managers.php
+++ view-managers.php
@@ -20,7 +20,7 @@ while ($manager=$managers->fetch_assoc()) {
     <td><?php echo $manager['description']; ?></td>
     <td><?php echo $manager['manager_number']; ?></td>
     <td>
-      <form method="post" action="animals-by-managers.php">
+      <form method="post" action="view-animals-by-managers.php">
         <input type="hidden" name="cid" value="<?php echo $manager['manager_id']; ?>">
         <button type="submit" class="btn btn-primary">Animals</button>
 </form>
