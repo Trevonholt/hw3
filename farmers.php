04d5801a971b511fcd724304d0f6a8bac4425143
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 14:57:35 -0500

    Update farmers.php

--- farmers.php
+++ farmers.php
@@ -4,6 +4,14 @@ require_once("model-farmers.php");
 
 $pageTitle="Farmers";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']){
+    case "Add": 
+      insertFarmers($_POST['fName'], $_POST['phoneN'], $_POST['email']);
+      break;
+  }
+}
 $farmers=selectFarmers();
 include "view-farmers.php";
 include "view-footer.php";
