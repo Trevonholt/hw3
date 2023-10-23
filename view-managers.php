dba59fe5bae7dded70138e4cf3a3304c5c7d57e3
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 17:56:50 -0500

    Update view-managers.php

--- view-managers.php
+++ view-managers.php
@@ -1,4 +1,13 @@
-<h1>Managers</h1>
+<div class="row">
+  <div class="col">
+    <h1>Managers</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "view-managers-newform.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
