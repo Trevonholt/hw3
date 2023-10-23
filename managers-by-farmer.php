46c8695db14d045b8ba74a781056870c6fb0e3e9
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 09:07:09 -0500

    Create managers-by-farmer.php

---
+++ managers-by-farmer.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-managers-by-farmer.php");
+
+$pageTitle="Managers for Farmers";
+include "view-header.php";
+$managers=selectManagersByFarmer($_GET['id']);
+include "view-managers-by-farmer.php";
+include "view-footer.php";
+?>
