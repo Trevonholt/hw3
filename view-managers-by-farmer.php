eb4943801148d0400e1922a02277eb756572f06a
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 09:32:35 -0500

    Create view-managers-by-farmer.php

---
+++ view-managers-by-farmer.php
@@ -0,0 +1,31 @@
+<h1>Managers for Farmers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Phone Number</th>
+      <th>Type of Animals</th>
+      <th>Number of Animals</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($manager=$managers->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $manager['manager_id']; ?></td>
+    <td><?php echo $manager['manager_name']; ?></td>
+    <td><?php echo $manager['description']; ?></td>
+    <td><?php echo $manager['manager_number']; ?></td>
+    <td><?php echo $manager['type']; ?></td>
+    <td><?php echo $manager['count']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
