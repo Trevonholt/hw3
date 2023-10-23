28e1c79a2452942efbd78d4d659fff60e399662f
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 13:59:09 -0500

    Update model-farmers.php

--- model-farmers.php
+++ model-farmers.php
@@ -12,4 +12,47 @@ function selectFarmers() {
         throw $e;
     }
 }
+
+
+function insertFarmers($fName,$phoneN,$email) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `farmer` ( `farmer_name`, `phone_number`, `email`) VALUES (?,?, ?)");
+        $stmt->bind_param("sss", $fName, $phoneN, $email);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateFarmer($fName,$phoneN,$email,$Fid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `farmer` set `farmer_name`=?, `phone_number`=?, `email`=?) where farmer_id=?");
+        $stmt->bind_param("sssi", $fName, $phoneN, $email, $Fid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteFarmers($Fid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from farmer where farmer_id=?");
+        $stmt->bind_param("i", $Fid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
