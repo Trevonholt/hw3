2e96037411d1c4375383c55ca02e5ef52a3c4b9a
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 13:00:51 -0500

    Update view-farmers-with-managers.php

--- view-farmers-with-managers.php
+++ view-farmers-with-managers.php
@@ -7,13 +7,16 @@ while ($farmer=$farmers->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $farmer['farmer_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">    
 <?php
   $managers= selectManagersByFarmer($farmer['farmer_id']);
   while ($manager = $managers->fetch_assoc()) {
 ?>
+      <li class="list-group-item"><?php echo $manager['manager_name']; ?>- <?php echo $manager['description']; ?>-<?php echo $manager['manager_number']; ?></li>
 <?php
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Phone Number: <?php echo $farmer['phone_number']; ?></small></p>
     </div>
