4a26494090ecde80140c15d22163c5ff4bfc778c
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 09:37:57 -0500

    Update managers-by-farmer.php

--- managers-by-farmer.php
+++ managers-by-farmer.php
@@ -4,7 +4,7 @@ require_once("model-managers-by-farmer.php");
 
 $pageTitle="Managers for Farmers";
 include "view-header.php";
-$managers=selectManagersByFarmer($_GET['id']);
+$managers=selectManagersByFarmer($_GET['iid']);
 include "view-managers-by-farmer.php";
 include "view-footer.php";
 ?>
