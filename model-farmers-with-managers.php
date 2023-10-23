b614772013e555ca7b6170ea8b901bf3a352a18c
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 11:16:36 -0500

    Update model-farmers-with-managers.php

--- model-farmers-with-managers.php
+++ model-farmers-with-managers.php
@@ -12,4 +12,19 @@ function selectFarmers() {
         throw $e;
     }
 }
+
+function selectManagersByFarmer($iid) {
+        try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT m.manager_id, manager_name, description, manager_number, type, count FROM `manager`m JOIN animal a on m.manager_id=a.manager_id WHERE a.farmer_id=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
