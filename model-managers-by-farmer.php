04df8681151fe6cbe665f78cf02dca11d5e5f234
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 09:28:09 -0500

    Create model-managers-by-farmer.php

---
+++ model-managers-by-farmer.php
@@ -0,0 +1,16 @@
+<?php
+function selectManagersByFarmer($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT manager_id, manager_name, description, manager_number FROM `manager`SELECT m.manager_id, manager_name, description, manager_number, type, count  FROM `manager`m JOIN animal a on m.manager_id=a.manager_id WHERE a.farmer_id=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
