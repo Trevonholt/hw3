0b2f3a336d0f59465df2bff6ace89632bd79a415
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-23 09:52:52 -0500

    Update model-managers-by-farmer.php

--- model-managers-by-farmer.php
+++ model-managers-by-farmer.php
@@ -2,8 +2,8 @@
 function selectManagersByFarmer($iid) {
         try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT manager_id, manager_name, description, manager_number FROM `manager`SELECT m.manager_id, manager_name, description, manager_number, type, count  FROM `manager`m JOIN animal a on m.manager_id=a.manager_id WHERE a.farmer_id=?");
-        $stmt->bind_param("b", $iid);
+        $stmt = $conn->prepare("SELECT manager_id, manager_name, description, manager_number FROM `manager`SELECT m.manager_id, manager_name, description, manager_number, type  FROM `manager`m JOIN animal a on m.manager_id=a.manager_id WHERE a.farmer_id=?");
+        $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
