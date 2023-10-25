84a220dee2ece761315fd9079d104bebdfe6f54e
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-25 14:29:33 -0500

    Update farmers-with-managers.php

--- farmers-with-managers.php
+++ farmers-with-managers.php
@@ -2,27 +2,27 @@
 require_once("util-db.php");
 require_once("model-farmers-with-managers.php");
 
-$pageTitle="Farmers with Managers";
+$pageTitle = "Farmers with Managers";
 include "view-header.php";
 
-if (isset($_POST['actionType'])){
-  switch ($_POST['actionType']){
-    case "Add": 
-      if (insertMans($_POST['iid'],  $_POST['bid'], $_POST['mid'], $_POST['type'], $_POST['count'])) {
-        echo '<div class="alert alert-success" role="alert">Animal added!</div>';
-      } else{
-        echo '<div class="alert alert-danger" role="alert">Error!</div>';
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertAnimal($_POST['iid'],  $_POST['bid'], $_POST['mid'], $_POST['type'], $_POST['count'])) {
+        echo '<div class="alert alert-success" role="alert">Animal added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
-        case "Edit": 
-      if (updateMans($_POST['iid'], $_POST['bid'], $_POST['mid'], $_POST['type'], $_POST['count'], $_POST['aid'])) {
-        echo '<div class="alert alert-success" role="alert">Animal edited!</div>';
-      } else{
-        echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    case "Edit":
+      if (updateAnimal($_POST['iid'],  $_POST['bid'], $_POST['mid'], $_POST['type'], $_POST['count'], $_POST['aid'])) {
+        echo '<div class="alert alert-success" role="alert">Animal edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
-      if (deleteMans($_POST['aid'])) {
+      if (deleteAnimal($_POST['aid'])) {
         echo '<div class="alert alert-success" role="alert">Animal deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
@@ -31,7 +31,7 @@ if (isset($_POST['actionType'])){
   }
 }
 
-$farmers=selectFarmers();
+$farmers = selectFarmers();
 include "view-farmers-with-managers.php";
 include "view-footer.php";
 ?>
