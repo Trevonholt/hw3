cd091c05519c60fd524d628eb688bff345881e9b
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-25 09:32:35 -0500

    Update view-mans-editform.php

--- view-mans-editform.php
+++ view-mans-editform.php
@@ -11,7 +11,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editMansModalLabel<?php echo $animal['animal_id']; ?>">Edit managers</h1>
+        <h1 class="modal-title fs-5" id="editMansModalLabel<?php echo $animal['animal_id']; ?>">Edit Manager</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
