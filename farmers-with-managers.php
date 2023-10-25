0f158fe9753f45b6a556111fa1f9968a04f49026
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-25 07:55:16 -0500

    Update farmers-with-managers.php

--- farmers-with-managers.php
+++ farmers-with-managers.php
@@ -4,6 +4,33 @@ require_once("model-farmers-with-managers.php");
 
 $pageTitle="Farmers with Managers";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']){
+    case "Add": 
+      if (insertMans($_POST['fid'], $_POST['mid'], $_POST['mName'], $_POST['mNum'])) {
+        echo '<div class="alert alert-success" role="alert">Manager added!</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error!</div>';
+      }
+      break;
+        case "Edit": 
+      if (updateBreeds($_POST['type'], $_POST['breed'], $_POST['gender'], $_POST['bid'])) {
+        echo '<div class="alert alert-success" role="alert">Breed edited!</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error!</div>';
+      }
+      break;
+    case "Delete": 
+      if (deleteBreeds($_POST['bid'])) {
+        echo '<div class="alert alert-success" role="alert">Breed deleted!</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error!</div>';
+      }
+      break;
+  }
+}
+
 $farmers=selectFarmers();
 include "view-farmers-with-managers.php";
 include "view-footer.php";
