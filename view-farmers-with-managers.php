ed4fe917dd0d28954e28c4e7c10b514c6ad9b50d
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-10-25 19:20:31 -0500

    Update view-farmers-with-managers.php

--- view-farmers-with-managers.php
+++ view-farmers-with-managers.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="col">
-<h1>Farmers with Managers </h1>
+    <h1>Farmers with Managers</h1>
   </div>
   <div class="col-auto">
 <?php
@@ -10,41 +10,34 @@ include "view-farmers-with-managers-newform.php";
 </div>
 <div class="card-group">
 <?php
-while ($farmer = $farmers->fetch_assoc()) {
+while ($farmer=$farmers->fetch_assoc()) {
 ?>
-  <div class="card">
+    <div class="card">
     <div class="card-body">
       <h5 class="card-title"><?php echo $farmer['farmer_name']; ?></h5>
       <p class="card-text">
-      <ul class="list-group">
+      <ul class="list-group">    
 <?php
-  $managers = selectManagersByFarmer($farmer['farmer_id']);
+  $managers= selectManagersByFarmer($farmer['farmer_id']);
   while ($manager = $managers->fetch_assoc()) {
 ?>
-    <li class="list-group-item">
-    <div class="row">
-      <div class="col">
-        <?php echo $manager['manager_name']; ?>-<?php echo $manager['breed_id']; ?>- <?php echo $manager['type']; ?>-<?php echo $manager['count']; ?>>
-      </div>
-      <div class="col-auto">
-<?php
-include "view-farmers-with-managers-editform.php";
-?>
-      </div>
-      <div class="col-auto">
-        <form method="post" action="">
-          <input type="hidden" name="aid" value="<?php echo $animal['animal_id']; ?>">
-          <input type="hidden" name="actionType" value="Delete">
-          <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+      <li class="list-group-item"><?php echo $manager['manager_name']; ?>-<?php echo $manager['breed_id']; ?>- <?php echo $manager['type']; ?>-<?php echo $manager['count']; ?>
+        <td> 
+          
+        </td>
+        <td> 
+          <form method="post" action="">
+        <input type="hidden" name="aid" value="<?php echo $animal['animal_id']; ?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
               <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
             </svg>
-          </button>
-        </form>
-      </div>
-    </div>
-    </li>
+        </button>
+      </form>
+        </td>
+      </li>
 <?php
   }
 ?>
