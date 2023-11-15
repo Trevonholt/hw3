47072cfbfe93d3445160a38bd4dc33db4a623a66
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-15 10:55:22 -0600

    Update view-farmers-chart-3.php

--- view-farmers-chart-3.php
+++ view-farmers-chart-3.php
@@ -2,7 +2,7 @@
 <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 <div style="margin-top:16px;color:dimgrey;font-size:9px;font-family: Verdana, Arial, Helvetica, sans-serif;text-decoration:none;">Source: <a href="https://canvasjs.com/html5-javascript-column-chart/" target="_blank" title="JavaScript Column Charts &amp; Graphs ">https://canvasjs.com/html5-javascript-column-chart/</a></div>
 
-
+<script>
 window.onload = function () {
 
 var chart = new CanvasJS.Chart("chartContainer", {
@@ -34,3 +34,4 @@ var chart = new CanvasJS.Chart("chartContainer", {
 chart.render();
 
 }
+</script>
