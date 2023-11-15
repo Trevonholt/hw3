8b9ef4f280f4bcd0cb52130790d101eced88b4bd
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-15 00:53:38 -0600

    Update view-farmers-chart.php

--- view-farmers-chart.php
+++ view-farmers-chart.php
@@ -101,7 +101,14 @@ while ($farmer=$farmers->fetch_assoc()) {
   echo $farmer['num_managers'] . ", ";
 }
 ?>
-          ]
-    },
-    });
+          ],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(75, 192, 192)',
+      'rgb(255, 205, 86)',
+      'rgb(201, 203, 207)',
+      'rgb(54, 162, 235)'
+    ]
+  }]
+};
 </script>
